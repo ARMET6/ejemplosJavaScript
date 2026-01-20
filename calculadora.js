ce88e192fe79ede34688af518dfb0c09f3469697
ARMET6 <adrian.razo.mandujano.11@gmail.com>
2026-01-20 15:11:11 -0600

    A

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+// alert("Hola mundo hermoso y maravilloso");
+
+function mostrar (){
+    let numero1 = parseInt (document.getElementById("numero1").value);
+    let numero2 = parseInt (document.getElementById("numero2").value);
+
+    let resultado = 0;
+    resultado = numero1 + numero2;
+
+    let objResultado = document.getElementById("resultado");
+    objResultado.innerHTML = resultado
+
+    resultado.innerHTML = resultado;
+}
