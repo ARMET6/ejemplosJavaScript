88d435b512203d69a26a8246e532a8703fc9716b
ARMET6 <adrian.razo.mandujano.11@gmail.com>
2026-01-21 14:27:15 -0600

    operaciones

--- calculadora.js
+++ calculadora.js
@@ -1,14 +1,37 @@
-// alert("Hola mundo hermoso y maravilloso");
-
-function mostrar (){
-    let numero1 = parseInt (document.getElementById("numero1").value);
-    let numero2 = parseInt (document.getElementById("numero2").value);
-
+// comentario comentado
+alert("Adios mundo cruel")
+function mostrar(e) {
+    let numero1 = parseInt(document.getElementById("numero1").value);
+    let numero2 = parseInt(document.getElementById("numero2").value);
+    let result = document.getElementById("resultado");
     let resultado = 0;
-    resultado = numero1 + numero2;
+    let operacion = e.target.id;
+ 
+    if (operacion == "suma"){
+     resultado = numero1 + numero2;   
+    }
+    
+    else if (operacion == "resta"){
+        resultado = numero1 - numero2;
+    }
 
-    let objResultado = document.getElementById("resultado");
-    objResultado.innerHTML = resultado
+    else if (operacion == "multiplica"){
+        resultado = numero1 * numero2;
+    }
+    else if (operacion == "divide"){
+        resultado = numero1 / numero2;
+    }   
 
-    resultado.innerHTML = resultado;
+    else  {
+        alert("Funcionalidad en progreso")
+    }
+    
+
+    
+    
+
+    result.innerHTML = resultado;
+
 }
+let botones = document.getElementById("btns");
+botones.addEventListener("click", mostrar);
